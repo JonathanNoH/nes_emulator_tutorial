a61641f53f8267d773632d509870a50d9d6dd719
Jonathan NoH <97708227+JonathanNoH@users.noreply.github.com>
2023-03-17 18:04:50 -0600

    implement ability to run with callback before each game loop

--- src/cpu.rs
+++ src/cpu.rs
@@ -104,6 +104,13 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
+        self.run_with_callback(|_| {});
+    }
+
+    pub fn run_with_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU)
+    {
         let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
         loop {
             let code = self.mem_read(self.program_counter);
